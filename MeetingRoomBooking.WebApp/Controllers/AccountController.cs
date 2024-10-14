f831b91913a059fea0834a11935f5755bf7824b6
VinceYgot <vinceedrianygot@gmail.com>
2024-10-14 22:47:59 +0800

    Revert "made a forgot password function"
    This reverts commit 8bba58af702f22dbd868665faa5a1aed6b4417f9.

--- MeetingRoomBooking.WebApp/Controllers/AccountController.cs
+++ MeetingRoomBooking.WebApp/Controllers/AccountController.cs
@@ -5,19 +5,19 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using MeetingRoomBooking.WebApp.Models;
 using MeetingRoomBooking.Data;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using MeetingRoomBooking.Services.Manager;
-using Microsoft.EntityFrameworkCore;
-using MeetingRoomBooking.Services.ServiceModels;
 
 namespace MeetingRoomBooking.WebApp.Controllers {
     public class AccountController : Controller {
 
         private readonly MeetingRoomBookingDbContext _context;
 
-        public AccountController(MeetingRoomBookingDbContext meetingRoomBookingDbContext) {
+        public AccountController(MeetingRoomBookingDbContext meetingRoomBookingDbContext)
+        {
             _context = meetingRoomBookingDbContext;
         }
 
@@ -29,6 +29,36 @@ namespace MeetingRoomBooking.WebApp.Controllers {
         public IActionResult Login() {
             return View("~/Views/Account/Login.cshtml");
         }
+
+        //[HttpPost]
+        //public IActionResult Login(LoginViewModel model)
+        //{
+        //    if (ModelState.IsValid) {
+        //        var user = _context.Users.Where(x => (x.Email == model.Email || x.Phone == model.Email) 
+        //        && x.Password == model.Password && !x.Deleted)
+        //            .FirstOrDefault();
+        //        if (user != null) {
+        //            //login successfull, creating cookie
+        //            var claims = new List<Claim>
+        //            {
+        //                new Claim (ClaimTypes.Name, (user.FirstName + " " + user.LastName)),
+        //                new Claim("UserId", user.UserId.ToString()),
+        //                new Claim("Role", user.Role.ToString())
+        //            };
+
+        //            var claimsIdentity = new ClaimsIdentity (claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        //            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+        //            return RedirectToAction("Index", "Home");
+
+        //        }
+        //        else {
+        //            ModelState.AddModelError("", "Email or Password is not found");
+        //        }
+        //    }
+        //    return View(model);
+
+        //}
 
         [HttpPost]
         public IActionResult Login(LoginViewModel model) {
@@ -74,39 +104,15 @@ namespace MeetingRoomBooking.WebApp.Controllers {
             return RedirectToAction("Login");
         }
 
-        public IActionResult ForgotPassword() {
+        public IActionResult ForgotPassword()
+        {
             return View();
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> SubmitPassword(ForgotPasswordViewModel newPasswordModel) {
-            // Check if the new password and confirmation match
-            if (newPasswordModel.NewPassword != newPasswordModel.ConfirmNewPassword) {
-                ModelState.AddModelError("ConfirmNewPassword", "The new password and confirmation do not match.");
-            }
-
-            if (ModelState.IsValid) {
-                // Find the user by email
-                var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == newPasswordModel.Email);
-                if (user == null) {
-                    ModelState.AddModelError("Email", "No user found with that email address.");
-                    return View("ForgotPassword", newPasswordModel);
-                }
-
-                // Encrypt the new password and update the user's password
-                user.Password = PasswordManager.EncryptPassword(newPasswordModel.NewPassword);
-
-                // Save changes to the database
-                await _context.SaveChangesAsync();
-
-                // Redirect to the login page after successfully changing the password
-                return RedirectToAction("Login");
-            }
-
-            // If validation fails, return the view with the validation errors
-            return View("ForgotPassword", newPasswordModel);
-
+        public IActionResult SubmitPassword()
+        {
+            return View("~/Views/Account/Login.cshtml");
         }
     }
 }
