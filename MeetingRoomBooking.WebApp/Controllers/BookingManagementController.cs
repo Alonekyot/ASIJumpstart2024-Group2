590eb3f521c07cde1b0ad843a3d2e393c05222e2
VinceYgot <vinceedrianygot@gmail.com>
2024-11-22 21:38:46 +0800

    fixed endtime in calendar

--- MeetingRoomBooking.WebApp/Controllers/BookingManagementController.cs
+++ MeetingRoomBooking.WebApp/Controllers/BookingManagementController.cs
@@ -91,7 +91,7 @@ namespace MeetingRoomBooking.WebApp.Controllers {
                                {
                                    Title = booking.MeetingTitle + " - " + room.RoomName + "  (" + room.RoomLocation + ")",
                                    Start = booking.MeetingDate.ToDateTime(booking.StartTime),
-                                   End = booking.MeetingDate.ToDateTime(booking.StartTime),
+                                   End = booking.MeetingDate.ToDateTime(booking.EndTime),
                                    Description = room.RoomLocation
                                })
                          .ToList();
