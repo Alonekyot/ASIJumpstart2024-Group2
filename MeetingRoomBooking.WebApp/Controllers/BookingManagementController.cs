01eb100f31460ffac1a05f45957d8d0338a2dad7
Alonekyot <nalitanearl538@gmail.com>
2024-11-22 15:27:29 +0800

    Added a search button for the admindashboard, display the applicable bookings in the calendar

--- MeetingRoomBooking.WebApp/Controllers/BookingManagementController.cs
+++ MeetingRoomBooking.WebApp/Controllers/BookingManagementController.cs
@@ -4,6 +4,7 @@ using MeetingRoomBooking.Services.ServiceModels;
 using MeetingRoomBooking.WebApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace MeetingRoomBooking.WebApp.Controllers {
 
@@ -51,21 +52,21 @@ namespace MeetingRoomBooking.WebApp.Controllers {
             return View("Create", newBook);
         }
 
-        public JsonResult GetEvents(int roomId) {
+        public JsonResult GetEvents() {
+       
+            var events = _context.Bookings
+                    .Join(_context.Rooms,
+                               booking => booking.RoomId,
+                               room => room.RoomId,
+                               (booking, room) => new CalendarEvent
+                               {
+                                   Title = booking.MeetingTitle + " - " + room.RoomName,
+                                   Start = booking.MeetingDate.ToDateTime(booking.StartTime),
+                                   End = booking.MeetingDate.ToDateTime(booking.StartTime),
+                                   Description = room.RoomLocation
+                               })
+                         .ToList();
 
-            //var bookings = _bookingManager.GetBookings(roomId);
-
-            //var events = bookings.Select(b => new
-            //{
-            //    title = b.MeetingTitle,
-            //    start = b.MeetingDate.ToDateTime(b.StartTime),
-            //    end = b.MeetingDate.ToDateTime(b.EndTime),
-            //}).ToList();
-            var events = new List<CalendarEvent>
-            {
-                new CalendarEvent { Title = "Event 1", Start = DateTime.Today, End = DateTime.Today.AddDays(1) },
-                new CalendarEvent { Title = "Event 2", Start = new DateTime(2024, 11, 22, 8,0,0), End = new DateTime(2024, 11, 22, 12,0,0) },
-            };
             return new JsonResult(events);
         }
     }
